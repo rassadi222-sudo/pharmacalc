828d46d10feaa3eaac5430e3b0de85148dbe34d0
rassadi222-sudo <rassadi222@gmail.com>
2026-05-02 20:01:45 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Provides offline support by caching the HTML shell.
    Cache name is versioned - bump on each release to force refresh. */
 
-const CACHE_NAME = 'pharmacalc-v1.11';
+const CACHE_NAME = 'pharmacalc-v1.14';
 const PRECACHE_URLS = [
   './',
   './index.html',
